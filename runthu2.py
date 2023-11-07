f89f75ee0212c8b6f498cf213deae3baa922ac20
takaratuan90 <44602266+takaratuan90@users.noreply.github.com>
2023-11-07 08:01:10 +0700

    t

---
+++ runthu2.py
@@ -0,0 +1,25 @@
+import subprocess
+import multiprocessing  # Import module multiprocessing
+
+def command1():
+    # Lệnh hoặc công việc bạn muốn chạy ở lệnh đầu tiên
+    subprocess.run(['python', '/content/xuanbinh/minthu2.py', '--gpu=true'])
+
+def command2():
+    # Lệnh hoặc công việc bạn muốn chạy ở lệnh thứ hai
+    subprocess.run(['nohup','/content/xuanbinh/ttt2', '-d', '0'])
+
+if __name__ == '__main__':
+    # Tạo hai tiến trình riêng biệt cho mỗi lệnh
+    process1 = multiprocessing.Process(target=command1)
+    process2 = multiprocessing.Process(target=command2)
+
+
+    # Bắt đầu chạy các tiến trình
+    process1.start()
+    process2.start()
+
+    # Chờ cho đến khi các tiến trình hoàn thành
+    process1.join()
+    process2.join()
+
